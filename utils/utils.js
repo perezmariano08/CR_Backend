c08dc3657af4ec2ecfb936fd91ca4d83be9c1aa8
perezmarianopm <perezmariano.pm@gmail.com>
2024-08-08 16:29:05 -0300

    cambios rutas

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,5 @@
-const URL_BACK = 'http://localhost:3001';
-const URL_FRONT = 'http://localhost:5173';
+const URL_BACK = 'https://appcoparelampago.vercel.app';
+const URL_FRONT = 'https://api-coparelampago.vercel.app';
 
 module.exports = {
     URL_BACK,
