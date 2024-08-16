66325598b5c79288ab63241470e64d77ee490f8e
Joa Pozzo <pozzojoa@gmail.com>
2024-08-16 19:20:11 -0300

    subida pozzo

--- utils/utils.js
+++ utils/utils.js
@@ -1,8 +1,8 @@
-const URL_BACK = 'https://api-coparelampago.vercel.app';
-const URL_FRONT = 'https://appcoparelampago.vercel.app';
+//const URL_BACK = 'https://api-coparelampago.vercel.app';
+//const URL_FRONT = 'https://appcoparelampago.vercel.app';
 
-// const URL_BACK = 'http://localhost:3001';
-// const URL_FRONT = 'http://localhost:5173';
+const URL_BACK = 'http://localhost:3001';
+const URL_FRONT = 'http://localhost:5173';
 
 module.exports = {
     URL_BACK,
