88f09173da4de759bd7f15fb429697c0787107c8
Joa Pozzo <pozzojoa@gmail.com>
2024-08-30 18:55:57 -0300

    pruevas variables env

--- utils/db.js
+++ utils/db.js
@@ -2,18 +2,18 @@ const mysql = require('mysql');
 const dotenv = require('dotenv');
 const fs = require('fs');
 
-// dotenv.config();
+dotenv.config();
 
 const pool = mysql.createPool({
-  connectionLimit: 20,
-  host: 'srv1196.hstgr.io',
-  user: 'u436441116_mariano',
-  password: 'H^0enpzw',
-  database: 'u436441116_database',
-  connectTimeout: 60000,
-  acquireTimeout: 60000,
-  timeout: 60000,
-  flags: 'keep-alive'
+    connectionLimit: 20,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    connectTimeout: 60000,
+    acquireTimeout: 60000,
+    timeout: 60000,
+    flags: 'keep-alive'
 });
 
 // Configuración del archivo de registro de errores
