fb425521e96df70be2494d715c8545da5a6be095
Joa Pozzo <pozzojoa@gmail.com>
2024-09-04 00:01:33 -0300

    gola

--- controllers/authController.js
+++ controllers/authController.js
@@ -105,6 +105,7 @@ const checkLogin = (req, res) => {
     const { dni, password } = req.body;
     db.query('SELECT * FROM usuarios WHERE dni = ?', [dni], (err, rows) => {
         if (err) return res.status(500).send('Error interno del servidor');
+        
         if (rows.length === 0) return res.status(401).send('Usuario no encontrado');
 
         const user = rows[0];
