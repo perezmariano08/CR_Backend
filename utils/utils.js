c69fca1b75e551e7a5ab2cb0a8381d0302e96e05
Joa Pozzo <pozzojoa@gmail.com>
2024-09-13 14:29:57 -0300

    cambios url

--- utils/utils.js
+++ utils/utils.js
@@ -1,6 +1,6 @@
 const URL_BACK = 'https://api-coparelampago.vercel.app';
-// const URL_FRONT = 'https://prueba.coparelampago.com';
-const URL_FRONT = 'https://appcoparelampago.vercel.app';
+const URL_FRONT = 'https://coparelampago.com';
+// const URL_FRONT = 'https://appcoparelampago.vercel.app';
 
 // const URL_BACK = 'http://localhost:3001';
 // const URL_FRONT = 'http://localhost:5173';
